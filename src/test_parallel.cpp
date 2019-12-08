c2de39c1508073d47db9547d9569e48af02b1963
Orankarl <895168349@qq.com>
2019-12-08 11:34:14 -0500

    change test

--- src/test_parallel.cpp
+++ src/test_parallel.cpp
@@ -1,16 +1,21 @@
 #include "tree.h"
 
-// #include <iostream>
-// #include <fstream>
-// #include <stdlib.h>
-// #include <pthread.h>
-// #include <time.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <vector>
+#include <unistd.h>
 
 using namespace std;
+
+vector<float> COMP_TIME_LIST = {0, 0.0001, 0.0005, 0.001, 0.005, 0.01};
 
 int total_size = 0, size_per_thread = 0;
 int numbers[1000001];
 tree_node *root;
+int sleep_time = 0; // in usec
 
 void load_data_from_txt();
 int run_multi_thread(int thread_count);
@@ -39,7 +44,17 @@ int main()
     // root = rb_init();
     run_multi_thread(16);
     // root = rb_init();
-    // run_multi_thread(32);
+    run_multi_thread(32);
+
+    for (auto time : COMP_TIME_LIST) {
+        sleep_time = 1000000 * time;
+        root = rb_init();
+        run_multi_thread(1);
+        run_multi_thread(2);
+        run_multi_thread(4);
+        run_multi_thread(8);
+        run_multi_thread(16);
+    }
     return 0;
 }
 
@@ -67,6 +82,7 @@ void *run(void *p)
     {
         int element = start[i];
         rb_insert(root, element);
+        usleep(sleep_time);
         dbg_printf("[RUN] finish inserting element %d\n", element);
         // if (i > 100000) break;
     }
@@ -97,7 +113,8 @@ int run_multi_thread(int thread_count)
     double elapsed_time = (end.tv_sec - start.tv_sec) * 1e9;
     elapsed_time += (end.tv_nsec - start.tv_nsec);
     elapsed_time *= 1e-9;
-    cout << "time taken by program with " << thread_count + 1 << " threads: " << fixed << elapsed_time << "sec" << endl;
+    cout << "time taken by program with " << thread_count + 1 << " threads and sleep_time " << (float)sleep_time / 1000000 << " seconds: " << fixed << elapsed_time << "sec" << endl;
+    cout.unsetf(ios_base::floatfield);
 
     // show_tree(root);
     return 0;
